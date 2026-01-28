919c774cda22028892fd8d274086522b6b1da0c5
shakedshoshan-ort <ShakedS@admin.ort.org.il>
2026-01-28 11:57:15 +0200

    Remove pagination indicators from QuestionScreen for a cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -296,9 +296,7 @@ function QuestionScreen({ question, questionNumber, totalQuestions, answer, onSe
                 </button>
               ))}
             </div>
-            <div className="flex justify-between text-xs text-blue-400/50 px-2">
-              <span>1</span> <span>2</span> <span>3</span> <span>4</span> <span>5</span>
-            </div>
+            
           </div>
         </div>
 
